14bdaa2c704d67316c4cc30bc1952d6a3011732e
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2020-02-02 19:21:13 +0100

    Adjusted main program.

--- cmd/ggpack/main.go
+++ cmd/ggpack/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/s-l-teichmann/ggpack"
 )
+
+var listFiles = true
 
 func process(fname string) error {
 	file, err := os.Open(fname)
@@ -19,10 +24,35 @@ func process(fname string) error {
 	if err := reader.ReadPack(); err != nil {
 		return err
 	}
+
+	if listFiles {
+		files := reader.Entries().Find("files")
+		if files == nil || files.Type() != ggpack.ArrayType {
+			return errors.New("no files found")
+		}
+		stdout := bufio.NewWriter(os.Stdout)
+		fs := files.Array()
+		for _, f := range fs {
+			if f.Type() != ggpack.HashType {
+				continue
+			}
+			name := f.Find("filename")
+			size := f.Find("size")
+			if name != nil && size != nil &&
+				name.Type() == ggpack.StringType &&
+				size.Type() == ggpack.IntegerType {
+				fmt.Fprintf(stdout, "%s\t%d\n", name.String(), size.Integer())
+			}
+		}
+		if err := stdout.Flush(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func main() {
+	flag.BoolVar(&listFiles, "list", true, "list all files")
 	flag.Parse()
 
 	for _, arg := range flag.Args() {
